0286c7d03dba1d603edf8fda937486a66aa68418
Sam <63943054+invadingdingo@users.noreply.github.com>
2023-05-02 13:44:23 -0500

    Invert pizza slice

--- Assets/Scripts/Player/PizzaPulse.cs
+++ Assets/Scripts/Player/PizzaPulse.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class PizzaPulse : MonoBehaviour {
 
     public GameObject pizza;
-    //public BeatCounter beatC;
 
     void Start() {
-        // BeatCounter.instance.AddListener(OnBeat); 
         BeatCounter.instance.AddListener(OnBeat);
     }
     void OnBeat() {
-        Tween.Animate(this, 1f, 0.7f, 0.2f, Tween.EaseIn, s => {
+        Tween.Animate(this, 0.7f, 1f, 0.2f, Tween.EaseIn, s => {
             pizza.transform.localScale = new Vector3(s, s, 1f);
         });
     }
