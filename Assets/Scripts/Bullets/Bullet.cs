0c78ee7ae200649485287fc3e27ad188e6c48567
Oliver Berzs <oliver.berzs@gmail.com>
2023-05-04 22:41:47 +0300

    Add bullet hit effect

--- Assets/Scripts/Bullets/Bullet.cs
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
+    public GameObject BulletHit;
+
     void OnTriggerEnter2D(Collider2D other) {
+        Instantiate(BulletHit, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 }
